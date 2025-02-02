5ad891e9f98a8d6e836fb311c685b16bc5c595f0
Keshav Govind <dolphinboy12213@gmail.com>
2025-02-02 16:29:02 -0600

    ungerg bungerg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys, subprocess, os
 from colorama import init, Fore, Style
 
+init() # Initialize colorama
 
 def on_start():
     global character
@@ -10,24 +11,28 @@ def on_start():
 
 
 def main():
-    on_start()
-    with open("scripts/examplescript.txt", "r") as script:
-        for line in script:
-            if line.startswith():
-
-
-
+    while True:
+        homescreen()
+        scriptfile = file_select()
+        on_start()
+        with open(f"scripts/{scriptfile}", "r") as script:
+            for line in script:
+                if line.startswith(character):
+                    character_lines.append(line)
+            if not character_lines:
+                print(f"Oops! No lines found for {character}")
+    
 
 def homescreen():
     while True:
         subprocess.run('clear', shell=True)
-        print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX + "Welcome to Mad Libs!\n\n")
-        print(Fore.GREEN + "(P)lay", end="")
+        print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX + "Welcome to Scripter!\n\n")
+        print(Fore.GREEN + "(S)cript", end="")
         print(Fore.RED + "        (E)xit\n" + Fore.RESET)
         inp = input()
         match inp.lower():
-            case "p":
-                return file_select()
+            case "s":
+                return
             case "e":
                 subprocess.run('clear', shell=True)
                 sys.exit()
@@ -37,16 +42,16 @@ def homescreen():
 
 def file_select():
     subprocess.run('clear', shell=True)
-    folder = os.listdir('./stories/templates')
+    folder = os.listdir('./scripts')
     txtfiles = []
     for file in folder:
         if file.endswith(".txt"):
             txtfiles.append(file)
     if not txtfiles:
-        raise LookupError("No '.txt' files found in /project/stories/templates")
+        raise LookupError("No '.txt' files found in /scripts/")
     while True:
         i = 1
-        print(Fore.BLUE + "Choose from one the available stories:")
+        print(Fore.BLUE + "Choose from one the available scripts:")
         file_dictionary = {}
         for file in txtfiles:
             print(f"{i}. {file}")
